e1f023af1245d9de8d49224ab26654fd36a4f3f1
Nikola Nisevic <e1528584@student.tuwien.ac.at>
2022-02-06 20:12:16 +0100

    implemented options to login and logout and deleted unnecessary spec.ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthService} from './services/auth.serivce';
+import {HttpClient} from '@angular/common/http';
+import {CardService} from './services/card.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'creditCardList';
+
+  body: string = '{"username": "nikola", "token": "LAE2xIA2avaeor768enivaR3783583VAeVaea7ioaF"}';
+
+  constructor(private authService: AuthService,
+              private httpClient: HttpClient,
+              private cardService: CardService,
+              private router: Router) {}
+
+  login() {
+    this.authService.loginUser();
+  }
+
+  logout() {
+    this.authService.logoutUser();
+    this.router.navigateByUrl('/');
+  }
+
+  isLoggedIn() {
+    return this.authService.isLoggedIn();
+  }
 }
