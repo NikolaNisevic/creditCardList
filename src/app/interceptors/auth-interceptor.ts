e790d5adcd24f201f42515e35c0300bda4e97f17
Nikola Nisevic <e1528584@student.tuwien.ac.at>
2022-02-06 20:07:22 +0100

    implemented interceptors to be able to intercept incoming or outgoing requests.

---
+++ src/app/interceptors/auth-interceptor.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AuthService} from '../services/auth.serivce';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const authUri = 'https://credit-card-list.application.riecken.io/login';
+
+    if (req.url === authUri || this.authService.getToken() == null) {
+      return next.handle(req);
+    }
+
+    const authReq = req.clone({
+      headers: req.headers.set('Authorization', 'Bearer ' + this.authService.getToken())
+    });
+
+    return next.handle(authReq);
+  }
+}
